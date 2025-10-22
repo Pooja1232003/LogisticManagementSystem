64c4b81aed4cda0700c02718bba6a49fa18c6f5a
Pooja1232003 <poojanisenanayaka03@gmail.com>
2025-10-23 00:26:46 +0530

    Add user menu to main

--- main.c
+++ main.c
@@ -11,5 +11,31 @@
 int main()
 {
 
+    int choice;
+
+    do {
+        printf("\n===== LOGISTICS MANAGEMENT MENU =====\n\n");
+        printf("1. Add New Cities\n");
+        printf("2. List of Cities\n");
+        printf("3. Rename a City\n");
+        printf("4. Remove a City\n");
+        printf("5. Enter Distances\n");
+        printf("6. Show Distance Table\n");
+        printf("7. Display Vehicles\n");
+        printf("8. Handle Delivery Request\n");
+        printf("9. Show Completed Deliveries\n");
+        printf("10. Reports\n");
+        printf("0. Exit\n\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+
+
+
+    } while (choice != 0);
+
+
     return 0;
 }
+
+

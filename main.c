6d07bbf69894154cd5d349027b9ce4ac8c8053b0
Pooja1232003 <poojanisenanayaka03@gmail.com>
2025-10-23 00:17:19 +0530

    Set up C program with headers and constants

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_CITIES 30
+#define MAX_VEHICLES 3
+#define MAX_DELIVERIES 50
+#define FUEL_PRICE 310.0
+
+
+int main()
+{
+
+    return 0;
+}
